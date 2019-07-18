805ca2ef21d1a32b284f11110a289d55e479c8d8
CepCap <nikinoob@gmail.com>
2019-07-18 16:31:09 +0300

    basic user show controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  match '/signup',    to: 'users#new',    via: 'get'
+  match '/signup',  to: 'users#new',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  resources :users
   root to: 'static_pages#home'
 end
